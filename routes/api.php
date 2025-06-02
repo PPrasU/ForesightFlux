271bdfeb671bf8fec9bd12cb50b92409a094c427
Putra Prasetya Utama <putrap2410@gmail.com>
2025-06-02 08:50:34 +0700

    push 5

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,17 @@
 <?php
 
 use App\Http\Controllers\KrakenController;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/migrate-now', function () {
+    try {
+        Artisan::call('session:table');
+        Artisan::call('migrate', ['--force' => true]);
+        return 'Migration berhasil dijalankan.';
+    } catch (\Exception $e) {
+        return 'Migration gagal: ' . $e->getMessage();
+    }
+});
+
 
 Route::post('/kraken/fetch', [KrakenController::class, 'fetchOHLC']);
